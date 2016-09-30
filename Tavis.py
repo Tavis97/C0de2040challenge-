d5ddffa041d16774b211b4629027f775f22d3323
Tavis97 <Tavis.Thompson@morehouse.edu>
2016-09-30 19:12:40 -0400

    Update Tavis.py

--- Tavis.py
+++ Tavis.py
@@ -1,4 +1,5 @@
 import requests
 info_rmation = { 'github':"https://github.com/Tavis97/C0de2040challenge-", 'token':" http://challenge.code2040.org/api/register"}
-r = requests.put( "http://challenge.code2040.org/api/register", info_rmation)
+url = "http://challenge.code2040.org/api/register"
+r = requests.post( url , info_rmation)
 
